1a6d21fc67799eb511e5b2bb2309d582b46019e6
TomasLin <horsekit1982@gmail.com>
2024-03-06 23:12:07 +0800

    增加 postgresql 的 docker-compose 增加 gorm

---
+++ README.md
@@ -0,0 +1,19 @@
+# Golang gin example
+
+# 參考資料
+
+[go-gin-example](https://github.com/eddycjy/go-gin-example)
+
+[go-json-web-tokens](https://medium.com/%E4%BC%81%E9%B5%9D%E4%B9%9F%E6%87%82%E7%A8%8B%E5%BC%8F%E8%A8%AD%E8%A8%88/golang-json-web-tokens-jwt-olang-json-web-tokens-jwt-%E7%A4%BA%E7%AF%84-225b377e0f79)
+
+[gorm-PostgreSQL](https://www.sqliz.com/posts/golang-gorm-postgresql/)
+
+[mac-docker-app](https://docs.docker.com/desktop/install/mac-install/)
+
+[gorm](https://gorm.io/docs/)
+
+[gorm-Migration](https://gorm.io/docs/migration.html)
+
+[Atlas Integration](https://atlasgo.io/)
+
+[Atlas Versioned Migrations](https://atlasgo.io/concepts/declarative-vs-versioned#versioned-migrations)
--- main.go
+++ main.go
@@ -2,12 +2,108 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/horsekitlin/go-gin-postgresql-example/routers"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Username string `gorm:"unique"`
+	Email    string
+}
+
+func connectToPostgreSQL() (*gorm.DB, error) {
+	dsn := "user=postgres password=1234 dbname=postgres host=localhost port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func createUser(db *gorm.DB, user *User) error {
+	result := db.Create(user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func getUserByID(db *gorm.DB, userID uint) (*User, error) {
+	var user User
+	result := db.First(&user, userID)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+
+func updateUser(db *gorm.DB, user *User) error {
+	result := db.Save(user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func deleteUser(db *gorm.DB, user *User) error {
+	result := db.Delete(user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
 
 func main() {
 	routersInit := routers.InitRouter()
-	endPoint := fmt.Sprintf(":%d", 8080)
+	endPoint := fmt.Sprintf(":%d", 8888)
+
+	db, err := connectToPostgreSQL()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer db.Close()
+
+	// Perform database migration
+	// err = db.AutoMigrate(&User{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a user
+	newUser := &User{Username: "john_doe", Email: "john.doe@example.com"}
+	err = createUser(db, newUser)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Created User:", newUser)
+
+	// Query user by ID
+	userID := newUser.ID
+	user, err := getUserByID(db, userID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("User by ID:", user)
+
+	// Update user
+	user.Email = "updated_email@example.com"
+	err = updateUser(db, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Updated User:", user)
+
+	// Delete user
+	err = deleteUser(db, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Deleted User:", user)
+
 	routersInit.Run(endPoint)
 }

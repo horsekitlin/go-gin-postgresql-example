dbcd27dd120650af838162564431efae4f834c41
TomasLin <horsekit1982@gmail.com>
2024-03-15 17:16:04 +0800

    feat: 簡單版本的ws

--- ws/ServeInterface.go
+++ ws/ServeInterface.go
@@ -4,6 +4,4 @@ import "github.com/gin-gonic/gin"
 
 type ServeInterface interface {
 	RunWs(gin *gin.Context)
-	GetOnlineUserCount() int
-	GetOnlineRoomUserCount(roomId int) int
 }
--- ws/primary/start.go
+++ ws/primary/start.go
@@ -2,32 +2,19 @@ package primary
 
 import (
 	"github.com/horsekitlin/go-gin-postgresql-example/ws"
-	"github.com/horsekitlin/go-gin-postgresql-example/ws/go_ws"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
-// 定义 serve 的映射关系
+// 定義 serve 的映射關係
 var serveMap = map[string]ws.ServeInterface{
-	"Serve":   &ws.Serve{},
-	"GoServe": &go_ws.GoServe{},
+	"Serve": &ws.Serve{},
 }
 
 func Create() ws.ServeInterface {
-	// GoServe or Serve
-	_type := viper.GetString("app.serve_type")
-	return serveMap[_type]
+	return serveMap["Serve"]
 }
 
 func Start(gin *gin.Context) {
 	Create().RunWs(gin)
-}
-
-func OnlineUserCount() int {
-	return Create().GetOnlineUserCount()
-}
-
-func OnlineRoomUserCount(roomId int) int {
-	return Create().GetOnlineRoomUserCount(roomId)
 }
